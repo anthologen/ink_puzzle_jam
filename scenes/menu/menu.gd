b6e7a40d53396cf7f461fb78d85f62cdc575e0f6
Matthew Murphy <mashumafi@users.noreply.github.com>
2022-06-04 15:29:30 -0400

    Add Title and Drip animation (#26)

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,13 +1,21 @@
 extends Control
 
 
+func _input(event: InputEvent):
+	if event is InputEventKey or event is InputEventMouseButton:
+		var params = {
+			show_progress_bar = true,
+			"a_number": 10,
+			"a_string": "Ciao mamma!",
+			"an_array": [1, 2, 3, 4],
+			"a_dict": {"name": "test", "val": 15},
+		}
+		Game.change_scene("res://scenes/gameplay/gameplay.tscn", params)
+
+
 func _ready():
 	$Version/GameVersion.text = ProjectSettings.get_setting("application/config/version")
 	$Version/GodotVersion.text = "Godot %s" % Engine.get_version_info().string
-	# needed for gamepads to work
-	$VBoxContainer/PlayButton.grab_focus()
-	if OS.has_feature('HTML5'):
-		$VBoxContainer/ExitButton.queue_free()
 
 
 func pre_start(params):
@@ -25,24 +33,3 @@ func pre_start(params):
 			$CenterContainer/TitleVBox/Message.text = "You Won!"
 		else:
 			$CenterContainer/TitleVBox/Message.text = "You Lost!"
-
-
-func _on_PlayButton_pressed() -> void:
-	var params = {
-		show_progress_bar = true,
-		"a_number": 10,
-		"a_string": "Ciao mamma!",
-		"an_array": [1, 2, 3, 4],
-		"a_dict": {"name": "test", "val": 15},
-	}
-	Game.change_scene("res://scenes/gameplay/gameplay.tscn", params)
-
-
-func _on_ExitButton_pressed() -> void:
-	# gently shutdown the game
-	var transitions = get_node_or_null("/root/Transitions")
-	if transitions:
-		transitions.fade_in({'show_progress_bar': false})
-		yield(transitions.anim, "animation_finished")
-		yield(get_tree().create_timer(0.3), "timeout")
-	get_tree().quit()
