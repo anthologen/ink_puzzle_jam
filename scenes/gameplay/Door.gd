1b3f398424403c2619238c9e39caca74cd246950
Matthew Murphy <mashumafi@users.noreply.github.com>
2022-06-04 17:12:33 -0400

    Add second level, door is now generic (#29)

--- scenes/gameplay/Door.gd
+++ scenes/gameplay/Door.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+export (String, FILE, "*.tscn") var exit
+
 
 func _on_Door_body_entered(body: Node):
 	if body.name == "Player":
 		var params = {"is_win": true}
-		Game.change_scene("res://scenes/menu/menu.tscn", params)
+		Game.change_scene(exit if exit else "res://scenes/menu/menu.tscn", params)
