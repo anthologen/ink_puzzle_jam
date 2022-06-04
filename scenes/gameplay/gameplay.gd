15ec4dae5c3e8a8839dcd5e676e5a6bb1b82a86a
Matthew Murphy <mashumafi@users.noreply.github.com>
2022-06-03 23:27:54 -0400

    Add player with basic physics controller to game (#5)

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -12,7 +12,6 @@ func pre_start(params):
 		for key in params:
 			var val = params[key]
 			printt("", key, val)
-	$Sprite.position = Game.size / 2
 	print("Processing...")
 	yield(get_tree().create_timer(2), "timeout")
 	print("Done")
@@ -25,5 +24,3 @@ func start():
 
 func _process(delta):
 	elapsed += delta
-	$Sprite.position.x = Game.size.x / 2 + 150 * sin(2 * 0.4 * PI * elapsed)
-	$Sprite.position.y = Game.size.y / 2 + 100 * sin(2 * 0.2 *  PI * elapsed)
