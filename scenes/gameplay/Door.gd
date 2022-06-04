39f6a9ec8ef0767f363df601f71912d034ba6011
Megh Parikh <meghprkh@gmail.com>
2022-06-04 22:55:34 +0100

    Add sound effects

--- scenes/gameplay/Door.gd
+++ scenes/gameplay/Door.gd
@@ -5,5 +5,5 @@ export (String, FILE, "*.tscn") var exit
 
 func _on_Door_body_entered(body: Node):
 	if body.name == "Player":
-		var params = {"is_win": true}
-		Game.change_scene(exit if exit else "res://scenes/menu/menu.tscn", params)
+		body.level_win(exit)
+
